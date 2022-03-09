bb04c1d8f7718fb005648dd775503a50ee8bec62
AnaCouto1409 <35375044+AnaCouto1409@users.noreply.github.com>
2022-03-08 22:53:46 -0300

    Add files via upload

---
+++ aula6/main.js
@@ -0,0 +1,44 @@
+
+
+//titleReference.style.color='blue';
+
+//titleReference.style.fontSize='7rem';
+
+//console.log(titleReference)
+
+//let titleReference = document.querySelector('.title');
+
+//titleReference.classList.add('.title');
+
+let mainMenuReference = document.querySelector('.main-menu-opened')
+
+function openMenu(){
+    //texto de código separado
+    MenuReference.classList.add('main-menu-opened')
+}
+function closeMen(){
+    mainMenuReference.classList.add('main-menu-opened')
+}
+
+function manipularMenu(){
+    mainMenuReference.classList.toggle('main-menu')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
