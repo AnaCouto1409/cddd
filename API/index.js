4633f7fa827c46aa3e03b98dc6ac20ea558887f3
AnaCouto1409 <35375044+AnaCouto1409@users.noreply.github.com>
2022-03-15 16:33:13 -0300

    Add files via upload

---
+++ API/index.js
@@ -0,0 +1,8 @@
+function botao(){
+    document.getElementById("#nome").style.background="#cccccc"
+    document.getElementById("#nome").style.height="200px"
+    document.getElementById("#nome").style.width="200px"
+    var musica = document.querySelector("<p>");
+    musica.innerHTML ="Imprime ai para mim please";
+    
+}
---
+++ API/teste.js
@@ -0,0 +1,103 @@
+//Como tratar erros lançados????
+
+//vai chamar via prompt
+
+var  balas = Number(prompt("Digite o numero de balas que será dividido"));
+var crianças = Number(prompt("Digite o numero de crianças"));
+
+try{
+    if(balas > 0 && crinças > 0) 
+    var divisãoTotal = balas/crianças;
+    console.log("O numero de balas por crianças será de:" + divisaoTotal);
+} else{
+    alert("Por favor digite o numero de balas maior do que zero");
+}
+catch (erro){
+    console.log(" O numero de balas  não foi encontrado")
+}
+// catch e  utilizado para tratar erro de aplicação
+//de aplicação de sites
+//para entra dentro do catch e sempre erro de programação
+// atividade
+
+
+var array = [];
+class pilha {
+    constructor (pilha){
+        this.array = [];
+        this.pilha = [];
+    }
+    
+ constructor (empilha){
+        this.array =[];
+        this.array ="Empilha esse trem para mim";
+ }
+ constructor(desempilha){
+     this.array = [];
+     this.array = "Desempilha isso ai para mim please";
+ }
+}
+
+class pilha{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+    setNext (next){
+        this.text = next;
+    }
+    getValue(){
+        return this.value
+    }
+}
+let elemento = new pilha (10);
+let segundoElemento = new pilha;
+var num = [1,2,3,4];
+//metodo push para adicionar is elemento dentro de uma array
+//o metodo pop retirar e usar os elementos
+//da pilha utilizando o elemeto pop
+//num.pop
+//agora sim o exercicio
+   class  pilha{
+  constructor(value){
+         this.value = value;
+         this.empilhamento = [];
+        }
+        get empilhar(){
+            return this.pushValue();
+        }
+        pushValue(){
+       empilhamento.push(this.value);
+        }
+    }
+    let input = new pilha(10);
+     let segundoElemento= new pilha(25);
+    console.log(elemento.value);
+    console.log(segundoElemento.value);    
+//ichi pro
+
+   //function pilha(){
+     //  var arr = []
+      // this.empilhar = function (n)
+   //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
