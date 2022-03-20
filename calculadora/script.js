f120cca2ca8cfd25feda5c9467d63beaaca51a7a
AnaCouto1409 <35375044+AnaCouto1409@users.noreply.github.com>
2022-03-19 23:30:23 -0300

    Add files via upload

---
+++ calculadora/script.js
@@ -0,0 +1,28 @@
+(() => {
+    const resultado = document.querySelector('.h2');
+    // evento que identifica o botao Clean
+    document.querySelector('[data-clean=C]').addEventListener('click',clean);
+    
+    // evento que identifica o botao Del
+    document.querySelector('[data-clean=del]').addEventListener('click',del);
+    
+    // evento que identifica os dígito numérico e operação
+    const botoes = document.querySelectorAll('[data-option]');
+    botoes.forEach(botao => {
+        botao.addEventListener('click', () => {
+            erroDeCalculo(resultado.textContent);
+            limiteDeCaracteres(resultado.textContent);
+            const digito = botao.dataset.option;
+            resultado.innerHTML += digito;
+        });
+    });
+
+    // evento que executa o resultado da operação
+    document.querySelector('[data-resultado=calcular]').addEventListener('click', () => {
+        resultado.innerHTML = eval(resultado.textContent);
+        erroSemDigito(resultado.textContent);
+        erroNaN(resultado.textContent);
+    });
+})
+
+
